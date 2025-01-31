9089ba242674be2485abc4287e45f02e211aa40b
Sorin Covor <sorincovor@yahoo.com>
2025-01-31 22:26:45 +0200

    Another attempt at types

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({})
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'index.js'),
