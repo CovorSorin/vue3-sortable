4e6cfec55597d50e24f68811c441d906ce5ecbd9
Sorin Covor <sorincovor@yahoo.com>
2025-04-28 22:01:04 +0300

    Export moveArrayElement method

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import AppSortable from './src/components/AppSortable.vue'
+import { moveArrayElement } from './src/modules/utils/utils.js'
 
 export {
-  AppSortable
+  AppSortable,
+  moveArrayElement
 }
