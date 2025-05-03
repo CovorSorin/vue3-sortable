b85e5081bb215df495c73bd2a289cc2d347020cf
Sorin Covor <sorincovor@yahoo.com>
2025-05-03 22:48:48 +0300

    example

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -82,29 +82,28 @@ function addRandomHorizontalItem() {
   </div>
 
   <div style="position: relative">
-  <AppSortable
-    v-model="verticalList"
-    ref="verticalListRef"
-    class="sortable-vertical"
-    item-class="item"
-    :handle="handle"
-    direction="vertical"
-    transition-group-name="scale-y"
-    item-key="id"
-  >
-    <template #item="{ item, index }">
-      <div>
-        <div class="handle mr-8" :style="{ backgroundColor: item.color }"></div>
-        <div>{{ item.name }}</div>
-        <button @click="removeItem(verticalList, index)">Remove</button>
-      </div>
-    </template>
-  </AppSortable>
+    <AppSortable
+      v-model="verticalList"
+      ref="verticalListRef"
+      class="sortable-vertical"
+      item-class="item"
+      :handle="handle"
+      direction="vertical"
+      transition-group-name="scale-y"
+      item-key="id"
+    >
+      <template #item="{ item, index }">
+        <div>
+          <div class="handle mr-8" :style="{ backgroundColor: item.color }"></div>
+          <div>{{ item.name }}</div>
+          <button @click="removeItem(verticalList, index)">Remove</button>
+        </div>
+      </template>
+    </AppSortable>
 
-    <div style="position: absolute; top: 0; width: 100%; height: 50px; background: red; opacity: 0.3"></div>
-    <div style="position: absolute; bottom: 0; width: 100%; height: 50px; background: red; opacity: 0.3"></div>
+    <div v-if="false" style="position: absolute; top: 0; width: 100%; height: 50px; background: red; opacity: 0.3"></div>
+    <div v-if="false" style="position: absolute; bottom: 0; width: 100%; height: 50px; background: red; opacity: 0.3"></div>
   </div>
-
 
   <h3>Horizontal list</h3>
 
